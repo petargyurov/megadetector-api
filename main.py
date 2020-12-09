31358d129ac068be49a4b6544fa36db007be9084
Petar Gyurov <pgyurov93@gmail.com>
2020-12-09 14:09:33 +0000

    Updated main.py example

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from tf_detector import TFDetector
 
-model_file = 'md_v4.1.0.pb'
-tf_detector = TFDetector(model_file, output_path='/output')
+tf_detector = TFDetector(model_path='md_v4.1.0.pb', output_path='/output')
 
-results = tf_detector.run_detection(input_path='test_imgs/animals',
-                                    output_file='results.json')
+results = tf_detector.run_detection(input_path='test_imgs/stoats')
